fe1c9c666ec045f77f5e9fd6dcb4aa45271052a5
vkovec <viktoria.kovecses@gmail.com>
2017-07-31 14:58:42 -0400

    added unit tests
    - test for RemoveRedundantObjects
    - test for ConvertToValidFilename

--- Assets/FbxExporters/Editor/UnitTests/ModelExporterTest.cs
+++ Assets/FbxExporters/Editor/UnitTests/ModelExporterTest.cs
@@ -33,18 +33,31 @@ namespace FbxExporters.UnitTests
             }
         }
 
+        /// <summary>
+        /// Creates a GameObject.
+        /// Adds the object to the list of objects to be deleted
+        /// on TearDown.
+        /// </summary>
+        /// <returns>The game object.</returns>
+        /// <param name="name">Name.</param>
+        /// <param name="parent">Parent.</param>
+        /// <param name="type">Primitive Type.</param>
+        private GameObject CreateGameObject (string name, Transform parent = null, PrimitiveType type = PrimitiveType.Cube)
+        {
+            var go = GameObject.CreatePrimitive (type);
+            go.name = name;
+            go.transform.SetParent (parent);
+            m_createdObjects.Add (go);
+            return go;
+        }
 
         [Test]
         public void TestFindCenter ()
         {
             // Create 3 objects
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var cube1 = GameObject.CreatePrimitive (PrimitiveType.Cube);
-            var cube2 = GameObject.CreatePrimitive (PrimitiveType.Cube);
-
-            m_createdObjects.Add (cube);
-            m_createdObjects.Add (cube1);
-            m_createdObjects.Add (cube2);
+            var cube = CreateGameObject ("cube");
+            var cube1 = CreateGameObject ("cube1");
+            var cube2 = CreateGameObject ("cube2");
 
             // Set their transforms
             cube.transform.localPosition = new Vector3 (23, -5, 10);
@@ -58,6 +71,63 @@ namespace FbxExporters.UnitTests
 
             // Check that it is what we expect
             Assert.AreEqual(center, new Vector3(26, -2.5f, 6.75f));
+        }
+
+        [Test]
+        public void TestRemoveRedundantObjects ()
+        {
+            var root = CreateGameObject ("root");
+            var child1 = CreateGameObject ("child1", root.transform);
+            var child2 = CreateGameObject ("child2", root.transform);
+            var root2 = CreateGameObject ("root2");
+
+            // test set: root
+            // expected result: root
+            var result = FbxExporters.Editor.ModelExporter.RemoveRedundantObjects(new Object[]{root});
+            Assert.AreEqual (1, result.Count);
+            Assert.IsTrue (result.Contains (root));
+
+            // test set: root, child1
+            // expected result: root
+            result = FbxExporters.Editor.ModelExporter.RemoveRedundantObjects(new Object[]{root, child1});
+            Assert.AreEqual (1, result.Count);
+            Assert.IsTrue (result.Contains (root));
+
+            // test set: root, child1, child2, root2
+            // expected result: root, root2
+            result = FbxExporters.Editor.ModelExporter.RemoveRedundantObjects(new Object[]{root, root2, child2, child1});
+            Assert.AreEqual (2, result.Count);
+            Assert.IsTrue (result.Contains (root));
+            Assert.IsTrue (result.Contains (root2));
+
+            // test set: child1, child2
+            // expected result: child1, child2
+            result = FbxExporters.Editor.ModelExporter.RemoveRedundantObjects(new Object[]{child2, child1});
+            Assert.AreEqual (2, result.Count);
+            Assert.IsTrue (result.Contains (child1));
+            Assert.IsTrue (result.Contains (child2));
+        }
+
+        [Test]
+        public void TestConvertToValidFilename()
+        {
+            // test already valid filenames
+            var filename = "foobar.fbx";
+            var result = FbxExporters.Editor.ModelExporter.ConvertToValidFilename (filename);
+            Assert.AreEqual (filename, result);
+
+            filename = "foo_bar 1.fbx";
+            result = FbxExporters.Editor.ModelExporter.ConvertToValidFilename (filename);
+            Assert.AreEqual (filename, result);
+
+            // test invalid filenames
+            filename = "?foo**bar///.fbx";
+            result = FbxExporters.Editor.ModelExporter.ConvertToValidFilename (filename);
+            Assert.AreEqual ("_foo__bar___.fbx", result);
+
+            filename = "foo$?ba%r 2.fbx";
+            result = FbxExporters.Editor.ModelExporter.ConvertToValidFilename (filename);
+            Assert.AreEqual ("foo$_ba%r 2.fbx", result);
         }
     }
 }
