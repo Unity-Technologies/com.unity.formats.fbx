b4c928e0d5613d93be6fdbe8bde6b3e539e127e4
sinwood <simon.cf.inwood@gmail.com>
2017-08-08 14:02:06 -0400

    CR change

--- Assets/FbxExporters/Editor/ReviewLastSavedModel.cs
+++ Assets/FbxExporters/Editor/ReviewLastSavedModel.cs
@@ -5,7 +5,7 @@ namespace FbxExporters
 {
     namespace Review
     {
-        class TurnTable 
+        class TurnTable
         {
             const string MenuItemName = "FbxExporters/Turntable Review/Autoload Last Saved Prefab";
 
@@ -16,12 +16,12 @@ namespace FbxExporters
             static Object LastModel = null;
 
             [UnityEditor.MenuItem (MenuItemName, false, 10)]
-            public static void OnMenu()
+            public static void OnMenu ()
             {
-                LastSavedModel();
+                LastSavedModel ();
             }
 
-            private static System.IO.FileInfo GetLastSavedFile(string directoryPath, string ext = ".fbx")
+            private static System.IO.FileInfo GetLastSavedFile (string directoryPath, string ext = ".fbx")
             {
                 System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo (directoryPath);
                 if (directoryInfo == null || !directoryInfo.Exists)
@@ -31,34 +31,32 @@ namespace FbxExporters
                 System.DateTime recentWrite = System.DateTime.MinValue;
                 System.IO.FileInfo recentFile = null;
 
-                foreach (System.IO.FileInfo file in files)
-                {
-                    if (string.Compare(file.Extension, ext, System.StringComparison.OrdinalIgnoreCase)!=0) 
+                foreach (System.IO.FileInfo file in files) {
+                    if (string.Compare (file.Extension, ext, System.StringComparison.OrdinalIgnoreCase) != 0)
                         continue;
 
-                    if (file.LastWriteTime > recentWrite)
-                    {
+                    if (file.LastWriteTime > recentWrite) {
                         recentWrite = file.LastWriteTime;
                         recentFile = file;
                     }
                 }
-                return recentFile;                
+                return recentFile;
             }
 
-            private static string GetSceneFilePath()
+            private static string GetSceneFilePath ()
             {
                 return System.IO.Path.Combine (ScenesPath, SceneName + ".unity");
             }
 
-            private static string GetLastSavedFilePath()
+            private static string GetLastSavedFilePath ()
             {
-                string modelPath = FbxExporters.EditorTools.ExportSettings.GetAbsoluteSavePath();
+                string modelPath = FbxExporters.EditorTools.ExportSettings.GetAbsoluteSavePath ();
                 System.IO.FileInfo fileInfo = GetLastSavedFile (modelPath);
 
-                return (fileInfo!=null) ? fileInfo.FullName : null;
+                return (fileInfo != null) ? fileInfo.FullName : null;
             }
 
-            private static void UnloadModel(Object model)
+            private static void UnloadModel (Object model)
             {
                 if (model) {
                     GameObject unityGo = model as GameObject;
@@ -70,12 +68,12 @@ namespace FbxExporters
                 }
             }
 
-            private static Object LoadModel(string fbxFileName)
+            private static Object LoadModel (string fbxFileName)
             {
                 Object model = null;
 
                 // make relative to UnityProject folder.
-                string relFileName = System.IO.Path.Combine("Assets", FbxExporters.EditorTools.ExportSettings.ConvertToAssetRelativePath(fbxFileName));
+                string relFileName = System.IO.Path.Combine ("Assets", FbxExporters.EditorTools.ExportSettings.ConvertToAssetRelativePath (fbxFileName));
 
                 Object unityMainAsset = UnityEditor.AssetDatabase.LoadMainAssetAtPath (relFileName);
 
@@ -86,51 +84,97 @@ namespace FbxExporters
                 return model;
             }
 
-            private static void LoadLastSavedModel()
+            private static void LoadLastSavedModel ()
             {
-                string fbxFileName = GetLastSavedFilePath();
+                string fbxFileName = GetLastSavedFilePath ();
 
                 if (fbxFileName == null) return;
-                    
-                if (fbxFileName!=LastFilePath || LastModel==null)
-                {
-                    Object model = LoadModel(fbxFileName);
 
-                    if (model!=null)
-                    {
-                        if (LastModel!=null)
-                        {
-                            UnloadModel(LastModel);
+                if (fbxFileName != LastFilePath || LastModel == null) {
+                    Object model = LoadModel (fbxFileName);
+
+                    if (model != null) {
+                        if (LastModel != null) {
+                            UnloadModel (LastModel);
                         }
 
                         LastModel = model as Object;
                         LastFilePath = fbxFileName;
-                    }
-                    else
-                    {
-                        Debug.LogWarning(string.Format("failed to load model : {0}", fbxFileName));
+                    } else {
+                        Debug.LogWarning (string.Format ("failed to load model : {0}", fbxFileName));
                     }
                 }
             }
 
-            public static void LastSavedModel()
+            public static void LastSavedModel ()
             {
-                UnityEngine.SceneManagement.Scene scene = 
-                    UnityEngine.SceneManagement.SceneManager.GetSceneByName(SceneName);
+                UnityEngine.SceneManagement.Scene scene = new UnityEngine.SceneManagement.Scene();
 
-                if (!scene.IsValid ()) {
-                    scene = UnityEditor.SceneManagement.EditorSceneManager.NewScene(UnityEditor.SceneManagement.NewSceneSetup.DefaultGameObjects, 
-                                                                                    UnityEditor.SceneManagement.NewSceneMode.Additive);
-                    UnityEditor.SceneManagement.EditorSceneManager.SaveScene (scene, GetSceneFilePath ());
+                // get all scenes
+                System.Collections.Generic.List<UnityEngine.SceneManagement.Scene> scenes
+                      = new System.Collections.Generic.List<UnityEngine.SceneManagement.Scene> ();
+
+                for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++) {
+                    UnityEngine.SceneManagement.Scene toAdd = UnityEngine.SceneManagement.SceneManager.GetSceneAt (i);
+
+                    // skip Untitled scene. 
+                    // The Untitled scene cannot be unloaded, if modified, and we don't want to force the user to save it.
+                    if (toAdd.name == "") continue;
+
+                    if (toAdd.name == SceneName) 
+                    {
+                        scene = toAdd;
+                        continue;
+                    }
+                    scenes.Add (toAdd);
+                }
+
+                // if for some reason the turntable scene is missing create an empty scene
+                if (!scene.IsValid ()) 
+                {
+                    // create an empty scene
+                    // NOTE: we cannot use NewScene because it will force me to save the modified Untitled scene
+                    if (!System.IO.File.Exists(GetSceneFilePath ())) 
+                    {
+                        var writer = System.IO.File.CreateText (GetSceneFilePath ());
+                        writer.WriteLine ("%YAML 1.1\n%TAG !u! tag:unity3d.com,2011:");
+                        writer.Close ();
+                        UnityEditor.AssetDatabase.Refresh ();
+                    }
+
+                    scene = UnityEditor.SceneManagement.EditorSceneManager.OpenScene (GetSceneFilePath (), UnityEditor.SceneManagement.OpenSceneMode.Additive);
+                }
+
+                // save unmodified scenes
+                if (UnityEditor.SceneManagement.EditorSceneManager.SaveModifiedScenesIfUserWantsTo (scenes.ToArray ())) 
+                {
+                    // unload all scene except turntable & untitled
+                    for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++) {
+                        UnityEngine.SceneManagement.Scene toUnload = UnityEngine.SceneManagement.SceneManager.GetSceneAt (i);
+
+                        // skip Untitled scene
+                        if (toUnload.name == "")
+                            continue;
+
+                        // skip Turntable scene
+                        if (scene.Equals (toUnload))
+                            continue;
+                        
+                        UnityEditor.SceneManagement.EditorSceneManager.CloseScene (toUnload, false);
+                    }
+                } 
+                else
+                {
+                    Debug.Log ("Cannot enable turntable review when there are modified scenes");
+                    return;    
                 }
 
                 UnityEngine.SceneManagement.SceneManager.SetActiveScene (scene);
 
-                if (AutoUpdateEnabled ()) 
-                {
+                if (AutoUpdateEnabled ()) {
                     LoadLastSavedModel ();
 
-                    SubscribeToEvents();
+                    SubscribeToEvents ();
                 }
             }
 
@@ -143,7 +187,7 @@ namespace FbxExporters
 
             private static void UnsubscribeFromEvents ()
             {
-                UnloadModel(LastModel);
+                UnloadModel (LastModel);
 
                 LastModel = null;
                 LastFilePath = null;
@@ -151,21 +195,18 @@ namespace FbxExporters
                 UnityEditor.EditorApplication.hierarchyWindowChanged -= UpdateLastSavedModel;
             }
 
-            private static bool AutoUpdateEnabled()
+            private static bool AutoUpdateEnabled ()
             {
-                return (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == SceneName);
+                return (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == SceneName);
             }
 
-            private static void UpdateLastSavedModel()
+            private static void UpdateLastSavedModel ()
             {
 
-                if (AutoUpdateEnabled())
-                {
-                    LoadLastSavedModel();
-                }
-                else
-                {
-                    UnsubscribeFromEvents();
+                if (AutoUpdateEnabled ()) {
+                    LoadLastSavedModel ();
+                } else {
+                    UnsubscribeFromEvents ();
                 }
             }
         }
