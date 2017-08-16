99f95cfdcbbac8bfdf0a1f47fe6b22112d9115fa
vkovec <viktoria.kovecses@gmail.com>
2017-08-16 10:10:16 -0400

    fix tabs + add tooltip

--- Assets/FbxExporters/RotateModel.cs
+++ Assets/FbxExporters/RotateModel.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class RotateModel : MonoBehaviour {
 
-    public float speed = 10f;
-
-	void Update () {
-        transform.Rotate (Vector3.up, speed * Time.deltaTime);	
-	}
+    [Tooltip("Rotation speed in degrees/second")]
+    [SerializeField]
+    private float speed = 10f;
+    
+    void Update () {
+        transform.Rotate (Vector3.up, speed * Time.deltaTime, Space.World);	
+    }
 }
