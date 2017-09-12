cc97aabdf0e6db90fd4bcbdf3d618490cca7cb3b
vkovec <viktoria.kovecses@gmail.com>
2017-09-12 11:56:54 -0400

    handle multiple Unity projects open
    when bringing to front. Check that we are bringing the window with the
    correct open project to the front

--- Assets/Integrations/Autodesk/maya/scripts/unityOneClick/commands.py
+++ Assets/Integrations/Autodesk/maya/scripts/unityOneClick/commands.py
@@ -306,8 +306,10 @@ class reviewCmd(BaseCommand):
                 .format(unityAppPath, unityProjectPath, unityCommand)
 
         elif maya.cmds.about(windows=True):
-            melCommand = r'system("start \"{0}\" \"{1}\" \"-projectPath {2} -executeMethod {3}\"");'\
-                .format(unityProjectPath + "/Assets/Integrations/BringToFront.exe", unityAppPath, unityProjectPath, unityCommand)
+            melCommand = r'system("start \"{0}\" \"{1}\" \"{2}\" \"-projectPath {3} -executeMethod {4}\"");'\
+                .format(unityProjectPath + "/Assets/Integrations/BringToFront.exe", 
+                        os.path.basename(unityProjectPath), unityAppPath,
+                        unityProjectPath, unityCommand)
 
         else:
             raise NotImplementedError("missing platform implementation for {0}".format(maya.cmds.about(os=True)))
