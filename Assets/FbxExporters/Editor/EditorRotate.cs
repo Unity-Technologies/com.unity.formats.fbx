36f5490497e6e817bf3d2f68c973d2ff79e63742
vkovec <viktoria.kovecses@gmail.com>
2017-11-16 15:30:54 -0500

    remove cs headers
    - license file is enough

--- Assets/FbxExporters/Editor/EditorRotate.cs
+++ Assets/FbxExporters/Editor/EditorRotate.cs
@@ -1,12 +1,4 @@
-﻿// ***********************************************************************
-// Copyright (c) 2017 Unity Technologies. All rights reserved.
-//
-// Licensed under the ##LICENSENAME##.
-// See LICENSE.md file in the project root for full license information.
-// ***********************************************************************
-
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
