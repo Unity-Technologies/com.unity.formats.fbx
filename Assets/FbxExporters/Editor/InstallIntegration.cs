92debe21890f3b95fcc4aafe3c09465d58a24e9d
vkovec <viktoria.kovecses@gmail.com>
2017-11-16 12:43:53 -0500

    add missing headers

--- Assets/FbxExporters/Editor/InstallIntegration.cs
+++ Assets/FbxExporters/Editor/InstallIntegration.cs
@@ -1,3 +1,10 @@
+// ***********************************************************************
+// Copyright (c) 2017 Unity Technologies. All rights reserved.
+//
+// Licensed under the ##LICENSENAME##.
+// See LICENSE.md file in the project root for full license information.
+// ***********************************************************************
+
 using UnityEngine;
 using UnityEditor;
 using System;
