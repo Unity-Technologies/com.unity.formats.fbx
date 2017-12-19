826147c1d78723eee43c7c1d7fdded4f8dd412c8
sinwood <simon.cf.inwood@gmail.com>
2017-12-19 18:14:25 -0500

    Uni-33810_test_bobsfavouritedrive

--- Assets/FbxExporters/Editor/InstallIntegration.cs
+++ Assets/FbxExporters/Editor/InstallIntegration.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace FbxExporters.Editor
 {
-    abstract class DCCIntegration
+    public abstract class DCCIntegration
     {
         public abstract string DccDisplayName { get; }
         public abstract string IntegrationZipPath { get; }
@@ -82,7 +82,7 @@ namespace FbxExporters.Editor
     }
 
 
-    class MayaIntegration : DCCIntegration
+    public class MayaIntegration : DCCIntegration
     {
         public override string DccDisplayName { get { return "Maya"; } }
 
