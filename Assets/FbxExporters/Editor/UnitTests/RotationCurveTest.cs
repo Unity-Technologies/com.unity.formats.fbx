db4cc21f04973c2e9636e2c97a22a7c7cef422a3
vkovec <viktoria.kovecses@gmail.com>
2018-02-14 11:54:06 -0500

    test rotation curve basic functionality

--- Assets/FbxExporters/Editor/UnitTests/RotationCurveTest.cs
+++ Assets/FbxExporters/Editor/UnitTests/RotationCurveTest.cs
@@ -9,28 +9,34 @@ namespace FbxExporters.UnitTests
 {
     public class RotationCurveTest : ExporterTestBase {
 
-        private void TestRotationCurveBasics<T>() where T : RotationCurve {
-
-        }
-
-        [Test]
-        public void TestQuaternionBasics(){
-
-        }
-
     	[Test]
-    	public void TestEulerBasics() {
+    	public void TestBasics() {
             // Test get euler index
             var eulerCurve = new EulerCurve();
             Assert.That(EulerCurve.GetEulerIndex ("localEulerAnglesRaw.y"), Is.EqualTo(1));
             Assert.That(EulerCurve.GetEulerIndex ("localEulerAnglesRaw."), Is.EqualTo(-1));
-            Assert.That(EulerCurve.GetEulerIndex ("Quaternion.x"), Is.EqualTo(-1));
+            Assert.That(EulerCurve.GetEulerIndex ("m_LocalRotation.x"), Is.EqualTo(-1));
 
             // Test get quaternion index
+			var quaternionCurve = new QuaternionCurve();
+            Assert.That(QuaternionCurve.GetQuaternionIndex ("m_LocalRotation.w"), Is.EqualTo(3));
+            Assert.That(QuaternionCurve.GetQuaternionIndex ("m_LocalRotation"), Is.EqualTo(-1));
+            Assert.That(QuaternionCurve.GetQuaternionIndex ("localEulerAnglesRaw.y"), Is.EqualTo(-1));
 
             // Test SetCurve
+            var animCurve = new AnimationCurve();
 
+            eulerCurve.SetCurve (2, animCurve);
+            Assert.That (eulerCurve.m_curves [2], Is.EqualTo (animCurve));
 
+            Assert.That(() => eulerCurve.SetCurve (-1, animCurve), Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+            Assert.That(() => eulerCurve.SetCurve (3, animCurve), Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+
+            quaternionCurve.SetCurve (3, animCurve);
+            Assert.That (quaternionCurve.m_curves [3], Is.EqualTo (animCurve));
+
+            Assert.That(() => quaternionCurve.SetCurve (-5, animCurve), Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+            Assert.That(() => quaternionCurve.SetCurve (4, animCurve), Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
     	}
     }
 }
