8692fcf0c04ee2a5a3a3401fae0048035641b059
vkovec <viktoria.kovecses@gmail.com>
2019-05-01 11:22:00 -0400

    UT-2111 fix Show Convert UI tooltip

--- com.unity.formats.fbx/Editor/FbxExportSettings.cs
+++ com.unity.formats.fbx/Editor/FbxExportSettings.cs
@@ -71,7 +71,7 @@ namespace UnityEditor.Formats.Fbx.Exporter {
             EditorGUILayout.LabelField("Export Options", EditorStyles.boldLabel);
             EditorGUI.indentLevel++;
             exportSettings.ShowConvertToPrefabDialog = EditorGUILayout.Toggle(
-                new GUIContent("Show Convert UI:", "Enable Convert dialog when converting to a Linked Prefab"),
+                new GUIContent("Show Convert UI:", "Enable Convert dialog when converting to an FBX Linked Prefab"),
                 exportSettings.ShowConvertToPrefabDialog
             );
             EditorGUILayout.Space();
