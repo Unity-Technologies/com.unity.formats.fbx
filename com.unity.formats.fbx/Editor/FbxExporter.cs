05f04f5830421967e41cccac7ff5f8d06a950743
vkovec <viktoria.kovecses@imaginary-spaces.com>
2022-10-18 10:26:43 -0400

    change what is returned for vertex count

--- com.unity.formats.fbx/Editor/FbxExporter.cs
+++ com.unity.formats.fbx/Editor/FbxExporter.cs
@@ -4073,7 +4073,7 @@ namespace UnityEditor.Formats.Fbx.Exporter
             /// Gets the vertex count.
             /// </summary>
             /// <value>The vertex count.</value>
-            public int VertexCount { get { return mesh.vertexCount; } }
+            public int VertexCount { get { return m_vertices.Length; } }
 
             /// <summary>
             /// Gets the triangles. Each triangle is represented as 3 indices from the vertices array.
