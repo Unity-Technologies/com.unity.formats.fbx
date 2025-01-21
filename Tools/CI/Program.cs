1418ce48fe9e968e9e7cb58550636b2f9368499d
wind.xu <wind.xu@unity3d.com>
2025-01-21 10:42:41 -0500

    Add Wrench project

---
+++ Tools/CI/Program.cs
@@ -0,0 +1,21 @@
+﻿using fbxexporter.Cookbook.Settings;
+using RecipeEngine;
+using RecipeEngine.Modules.Wrench.Helpers;
+
+
+// ReSharper disable once CheckNamespace
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        var settings = new fbxexporterSettings();
+
+        // ReSharper disable once UnusedVariable
+        var engine = EngineFactory
+            .Create()
+            .ScanAll()
+            .WithWrenchModule(settings.Wrench)
+            .GenerateAsync().Result;
+        return engine;
+    }
+}
---
+++ Tools/CI/Settings/fbxexporterSettings.cs
@@ -0,0 +1,30 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace fbxexporter.Cookbook.Settings;
+
+public class fbxexporterSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"./com.unity.formats.fbx"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.formats.fbx",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public fbxexporterSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
