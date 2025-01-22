1dc43fa4b17f9e2d73204dbdd2463d060ddeb36c
windxu88 <45211214+windxu88@users.noreply.github.com>
2025-01-22 11:07:59 -0500

    Migrate to wrench release process (#739)
    * Add Wrench project
    
    * Only exclude .sln and .csproj files from TestProjects folder
    
    * Add missing .sln and .csproj files for Wrench project
    
    * Add prepack command

---
+++ Tools/CI/Settings/fbxexporterSettings.cs
@@ -0,0 +1,39 @@
+﻿using RecipeEngine.Api.Commands;
+using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace fbxexporter.Cookbook.Settings;
+
+public class fbxexporterSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"./com.unity.formats.fbx"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.formats.fbx",
+            new PackageOptions()
+            {
+                ReleaseOptions = new ReleaseOptions() { IsReleasing = true },
+                PackJobOptions = new PackJobOptions()
+                {
+                    PrePackCommands = new List<Command>
+                        {new Command("Build", "./build.sh")}
+                }
+            }
+        }
+    };
+
+    public fbxexporterSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
