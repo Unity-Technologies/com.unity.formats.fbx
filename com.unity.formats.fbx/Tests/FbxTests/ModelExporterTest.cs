0aded98b5b86c451a58f00212cb724b387b21a93
leonardo <leonardo@unity3d.com>
2026-06-05 13:45:45 +0200

    fixing obsolete FindObjectsSortMode warning in 6.4

--- com.unity.formats.fbx/Tests/FbxTests/ModelExporterTest.cs
+++ com.unity.formats.fbx/Tests/FbxTests/ModelExporterTest.cs
@@ -88,21 +88,17 @@ namespace FbxExporter.UnitTests
                 var character = new GameObject();
                 var smr = character.AddComponent<SkinnedMeshRenderer>();
                 smr.sharedMesh = cube.GetComponent<MeshFilter>().sharedMesh;
-#if UNITY_6000_6_OR_NEWER
+#if UNITY_6000_4_OR_NEWER
                 var meshCount = Object.FindObjectsByType<Mesh>().Length;
-#elif UNITY_2023_1_OR_NEWER
+#else
                 var meshCount = Object.FindObjectsByType<Mesh>(FindObjectsSortMode.None).Length;
-#else // UNITY_2023_1_OR_NEWER
-                var meshCount = Object.FindObjectsOfType<Mesh>().Length;
-#endif // UNITY_2023_1_OR_NEWER
+#endif // UNITY_6000_4_OR_NEWER
                 ModelExporter.ExportObject(GetRandomFbxFilePath(), character);
-#if UNITY_6000_6_OR_NEWER
+#if UNITY_6000_4_OR_NEWER
                 Assert.AreEqual(meshCount, Object.FindObjectsByType<Mesh>().Length);
-#elif UNITY_2023_1_OR_NEWER
+#else
                 Assert.AreEqual(meshCount, Object.FindObjectsByType<Mesh>(FindObjectsSortMode.None).Length);
-#else // UNITY_2023_1_OR_NEWER
-                Assert.AreEqual(meshCount, Object.FindObjectsOfType<Mesh>().Length);
-#endif // UNITY_2023_1_OR_NEWER
+#endif // UNITY_6000_4_OR_NEWER
             }
 
             // Test euler to quaternion conversion
